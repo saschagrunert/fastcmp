eafc49e8ddd200001d46f4111092ca8336ce081a
Sascha Grunert <Sascha.Grunert@rohde-schwarz.com>
2017-01-31 12:28:51 +0100

    Added two more benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -1,29 +1,51 @@
 #![feature(test)]
 extern crate test;
-extern crate fastcmp;
 
+extern crate fastcmp;
 use fastcmp::Compare;
 
 #[bench]
-fn slice_compare(b: &mut test::Bencher) {
+fn slice_compare_equal(b: &mut test::Bencher) {
     let vec1 = vec![5; 256];
     let vec2 = vec1.clone();
 
     b.iter(|| {
-        let (s1, s2) = (&vec1, &vec2);
-        s1 == s2
+        assert!(&vec2 == &vec1);
     });
     b.bytes = vec1.len() as u64;
 }
 
 #[bench]
-fn fast_compare(b: &mut test::Bencher) {
+fn fast_compare_equal(b: &mut test::Bencher) {
     let vec1 = vec![5; 256];
     let vec2 = vec1.clone();
 
     b.iter(|| {
-        let (s1, s2) = (&vec1, &vec2);
-        s1.feq(s2)
+        assert!(vec1.feq(&vec2));
     });
+    b.bytes = vec1.len() as u64;
+}
+
+#[bench]
+fn slice_compare_unequal(b: &mut test::Bencher) {
+    let vec = vec![5; 255];
+    let mut vec1 = vec.clone();
+    let mut vec2 = vec.clone();
+    vec1.push(1);
+    vec2.push(2);
+
+    b.iter(|| assert!(&vec1 != &vec2));
+    b.bytes = vec1.len() as u64;
+}
+
+#[bench]
+fn fast_compare_unequal(b: &mut test::Bencher) {
+    let vec = vec![5; 255];
+    let mut vec1 = vec.clone();
+    let mut vec2 = vec.clone();
+    vec1.push(1);
+    vec2.push(2);
+
+    b.iter(|| assert!(!vec1.feq(&vec2)));
     b.bytes = vec1.len() as u64;
 }
